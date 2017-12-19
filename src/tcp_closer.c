5a828eb9aabe3f356374ea8ef24d183a36a56e7a
Kristian Evensen <kristian.evensen@gmail.com>
2017-12-19 12:17:51 +0100

    tcp_closer: First version of filter code
    An early version of the filter generation code is working. It handles
    all test cases I can throw at it, but there is currently a lot of copy
    and paste.

--- src/tcp_closer.c
+++ src/tcp_closer.c
@@ -1,40 +1,186 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <linux/inet_diag.h>
 
 #include "tcp_closer.h"
 
-static bool parse_config_ports(int argc, char *argv[], uint16_t num_sport,
-                               uint16_t num_dport)
+static void output_filter(struct tcp_closer_ctx *ctx)
 {
-    return false;
+    uint16_t num_ops = ctx->diag_filter_len / sizeof(struct inet_diag_bc_op);
+    uint16_t i;
+
+    for (i = 0; i < num_ops; i++) {
+        fprintf(stdout, "diag_filter[%u]->code = %u\n", i,
+                ctx->diag_filter[i].code);
+        fprintf(stdout, "diag_filter[%u]->yes = %u\n", i,
+                ctx->diag_filter[i].yes);
+        fprintf(stdout, "diag_filter[%u]->no = %u\n", i,
+                ctx->diag_filter[i].no);
+        fprintf(stdout, "\n");
+    }
+}
+
+static void create_filter(int argc, char *argv[], struct tcp_closer_ctx *ctx,
+                          uint16_t num_sport, uint16_t num_dport)
+{
+    struct inet_diag_bc_op *diag_sports = ctx->diag_filter;
+    //Destination ports are always stored after source ports in our buffer
+    struct inet_diag_bc_op *diag_dports = ctx->diag_filter + (num_sport * 4);
+    uint16_t diag_sports_idx = 0, diag_dports_idx = 0, diag_sports_num = 1,
+             diag_dports_num = 1;
+    int opt;
+    struct option long_options[] = {
+        {"sport",   required_argument, NULL,   's'},
+        {"dport",   required_argument, NULL,   'd'},
+        {0,         0,                  0,       0 }
+    };
+
+    //According to the getopt man-page, this is the correct way to trigger a
+    //reset of the variables/values used by getopt_long.
+    optind = 0;
+
+    while ((opt = getopt_long(argc, argv, "s:d:", long_options, NULL)) != -1) {
+        //parse_cmdargs() is used to verify that input is valid. Here, we only
+        //care about the ports and do not need to handle, for example, unknown
+        //arguments
+        //TODO: Generalize handling of sport/dport once this is working
+        if (opt == 's') {
+            //Create the greater than operation. The only interesting thing
+            //going on is the value of no. If this is the last sport and
+            //we don't match, we should abort loop. Thus, we set it to 0xFFFF to
+            //ensure that len will be negative
+            diag_sports[diag_sports_idx].code = INET_DIAG_BC_S_GE;
+            diag_sports[diag_sports_idx].yes = sizeof(struct inet_diag_bc_op)
+                                                * 2;
+            diag_sports[diag_sports_idx].no = diag_sports_num == num_sport ?
+                                               0xFFFF :
+                                               sizeof(struct inet_diag_bc_op)
+                                               * 4;
+            diag_sports[diag_sports_idx + 1].code = INET_DIAG_BC_NOP;
+            diag_sports[diag_sports_idx + 1].no = atoi(optarg);
+
+            //Same as above. Here, yes is interesting. We can jump straight to
+            //dports. This means offset is sizeof() * 2 to pass this block.
+            //Then, we add sizeof() * 4 * (num_sport - diag_sports_num) to pass
+            //rest of sport comparisons
+            diag_sports[diag_sports_idx + 2].code = INET_DIAG_BC_S_LE;
+            diag_sports[diag_sports_idx + 2].yes = 
+                                                (sizeof(struct inet_diag_bc_op)
+                                                 * 2) +
+                                                (sizeof(struct inet_diag_bc_op)
+                                                 * 4 *
+                                                 (num_sport - diag_sports_num));
+            diag_sports[diag_sports_idx + 2].no =
+                                               diag_sports_num == num_sport ?
+                                               0xFFFF :
+                                               sizeof(struct inet_diag_bc_op)
+                                               * 2;
+            diag_sports[diag_sports_idx + 3].code = INET_DIAG_BC_NOP;
+            diag_sports[diag_sports_idx + 3].no = atoi(optarg);
+           
+            diag_sports_idx += 4;
+            diag_sports_num += 1;
+        } else if (opt == 'd') {
+            diag_dports[diag_dports_idx].code = INET_DIAG_BC_D_GE;
+            diag_dports[diag_dports_idx].yes = sizeof(struct inet_diag_bc_op)
+                                                * 2;
+            diag_dports[diag_dports_idx].no = diag_dports_num == num_dport ?
+                                               0xFFFF :
+                                               sizeof(struct inet_diag_bc_op)
+                                               * 4;
+            diag_dports[diag_dports_idx + 1].code = INET_DIAG_BC_NOP;
+            diag_dports[diag_dports_idx + 1].no = atoi(optarg);
+
+            //Same as above
+            diag_dports[diag_dports_idx + 2].code = INET_DIAG_BC_D_LE;
+            diag_dports[diag_dports_idx + 2].yes = 
+                                                (sizeof(struct inet_diag_bc_op)
+                                                 * 2) +
+                                                (sizeof(struct inet_diag_bc_op)
+                                                 * 4 *
+                                                 (num_dport - diag_dports_num));
+            diag_dports[diag_dports_idx + 2].no =
+                                               diag_dports_num == num_dport ?
+                                               0xFFFF :
+                                               sizeof(struct inet_diag_bc_op)
+                                               * 2;
+            diag_dports[diag_dports_idx + 3].code = INET_DIAG_BC_NOP;
+            diag_dports[diag_dports_idx + 3].no = atoi(optarg);
+           
+            diag_dports_idx += 4;
+            diag_dports_num += 1;
+        }
+    }
+
+    //fprintf(stdout, "diag_sports %p diag_dports %p\n", diag_sports, diag_dports);
+
+    //Iterate over arguments
+    //
+    //Create bc_op according to algorith, on paper
+    //
 }
 
 //Counts config ports and returns false if any unknown options is found
-static bool count_config_ports(int argc, char *argv[], uint16_t *num_sport,
-                               uint16_t *num_dport)
+static bool parse_cmdargs(int argc, char *argv[], uint16_t *num_sport,
+                          uint16_t *num_dport)
 {
-    int opt;
+    int opt, option_index;
     bool error = false;
 
-    while (!error && (opt = getopt(argc, argv, "s:d:h")) != -1) {
+    struct option long_options[] = {
+        {"sport",       required_argument,  NULL,   's'},
+        {"dport",       required_argument,  NULL,   'd'},
+        {"help",        required_argument,  NULL,   'h'},
+        {"kill_only",   no_argument,        NULL,    0 },   
+        {0,             0,                  0,       0 }
+    };
+
+    while (!error && (opt = getopt_long(argc, argv, "s:d:h", long_options,
+                                        &option_index)) != -1) {
         switch (opt) {
+        case 0:
+            fprintf(stdout, "Got option %s\n", long_options[option_index].name);
+            break;
         case 's':
-            (*num_sport)++;
+            if (!atoi(optarg)) {
+                fprintf(stderr, "Found invalid source port (value %s)\n",
+                        optarg);
+                error = true;
+            } else {
+                (*num_sport)++;
+            }
             break;
         case 'd':
-            (*num_dport)++;
+            if (!atoi(optarg)) {
+                fprintf(stderr, "Found invalid destination port (value %s)\n",
+                        optarg);
+                error = true;
+            } else {
+                (*num_dport)++;
+            }
             break;
         case 'h':
-            //Not really an error, but treat as such so that we call show_help()
-            error = true;
-            break;
         default:
-            fprintf(stderr, "Got unknown option %c\n", opt);
             error = true;
             break;
-        } 
+        }
+
+        //Perform limit check here to prevent a potential overflow when we
+        //create the buffers. A malicious user could for example provide 0xFFFF
+        //+ 1 source ports. If we do the check after loop, we would read number
+        //of source ports as one. However, when we create the filter, we will
+        //iterate over the arguments again and would then try to fit comparing
+        //0xFFFF + 1 ports into a buffer meant for one
+        if ((*num_sport + *num_dport) > MAX_NUM_PORTS) {
+            fprintf(stderr, "Number of ports (%u) exceeded limit (%u)\n",
+                *num_sport + *num_dport, MAX_NUM_PORTS);
+            error = true;
+            break;
+        }
     }
 
     return !error;
@@ -43,18 +189,21 @@ static bool count_config_ports(int argc, char *argv[], uint16_t *num_sport,
 static void show_help()
 {
     fprintf(stdout, "Following arguments are supported:\n");
-    fprintf(stdout, "\t-s : source port to match\n");
-    fprintf(stdout, "\t-d : destionation port to match\n\n");
+    fprintf(stdout, "\t-s/--sport : source port to match\n");
+    fprintf(stdout, "\t-d/--dport : destionation port to match\n");
+    fprintf(stdout, "\t-h/--help : This output\n");
+    fprintf(stdout, "\n");
     fprintf(stdout, "At least one source or destination port must be given.\n"
                     "We will kill connections where the source port is one of\n"
                     "the given source port(s) (if any), and the destination\n"
                     "port one of the given destination port(s) (if any).\n\n");
-    fprintf(stdout, "Maximum number of ports (combined) is %u\n",
+    fprintf(stdout, "Maximum number of ports (combined) is %u.\n",
             MAX_NUM_PORTS);
 }
 
 int main(int argc, char *argv[])
 {
+    struct tcp_closer_ctx *ctx = NULL;
     uint16_t num_sport = 0, num_dport = 0;
 
     //Parse options, so far it just to get sport and dport
@@ -63,13 +212,37 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    if (!count_config_ports(argc, argv, &num_sport, &num_dport)) {
+    //Parse options and count number of source ports/destination ports. We need
+    //to know the count before we create the filter, so that we can compute the
+    //correct offset for the different operations, etc.
+    if (!parse_cmdargs(argc, argv, &num_sport, &num_dport)) {
         show_help();
         return 1;
     }
 
     fprintf(stdout, "# source ports: %u # destination ports: %u\n", num_sport,
             num_dport);
+
+    ctx = calloc(sizeof(struct tcp_closer_ctx), 1);
+    if (!ctx) {
+        fprintf(stderr, "Failed to allocate memory for context-object\n");
+        return 1;
+    }
+
+    //Since there is no equal operator, a port comparison will requires four
+    //bc_op-structs. Two for LE (since ports is kept in a second struct) and two
+    //for GE
+    ctx->diag_filter_len = sizeof(struct inet_diag_bc_op) * 4 *
+                           (num_sport + num_dport);
+    ctx->diag_filter = calloc(ctx->diag_filter_len, 1);
+    if (!ctx->diag_filter) {
+        fprintf(stderr, "Failed to allocate memory for filter\n");
+        return 1;
+    }
+
+    create_filter(argc, argv, ctx, num_sport, num_dport);
+
+    output_filter(ctx);
 
     return 0;
 }
--- src/tcp_closer.h
+++ src/tcp_closer.h
@@ -1,6 +1,8 @@
 #ifndef TCP_CLOSER_H
 #define TCP_CLOSER_H
 
+struct inet_diag_bc_op;
+
 //When the kernel processes a filter (inet_diag_bc_run() in
 //net/ipv4/inet_diag.c), the first step is to read the length of the filter
 //(sizeof(struct inet_diag_bc_op) * num_elements_in_filter). The kernel then
@@ -16,14 +18,21 @@
 //The maximum offset that can be specified by one bc_op is 0xFFFF. To ensure
 //that we can always skip all pending bc_ops, the maximum length of the whole
 //filter is 0xFFFF - 1. One bc_op-struct is 4 bytes and, since there is no equal
-//code, we need to check both LE and GE for every port. Thus, comparing one port
-//consumed 8 bytes. The maximum number of bc_ops in our filter is therefore
-//(0xFFFF - 1) / 8 = 8191.
+//code, we need to check both LE and GE for every port. Finally, each comparison
+//operation consists of two bc-op-structs, since the port is kept in a second
+//bc_op-struct. Thus, comparing one port consumes 16 bytes. The maximum number
+//of bc_ops in our filter is therefore (0xFFFF - 1) / 16 = 4095.
 //
 //The limit can be solved in smarter ways, since we only need to be able to
 //abort the loop from the last member in the destination/source port sets.
-//However, that is an improvement for another day. 8000+ ports is enough for
+//However, that is an improvement for another day. 4000 ports is enough for
 //now.
-#define MAX_NUM_PORTS 8191
+#define MAX_NUM_PORTS 4095
+
+struct tcp_closer_ctx {
+    struct inet_diag_bc_op *diag_filter;
+
+    uint32_t diag_filter_len;
+};
 
 #endif
